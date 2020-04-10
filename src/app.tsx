b575cbd7da932b75bade021bc4e1dd07061ab436
Justin Wang <junting.wang@thoughtworks.com>
2020-04-10 16:06:59 +0800

    [Infra]: init project

---
+++ src/app.tsx
@@ -0,0 +1,36 @@
+import Taro, { Component, Config } from '@tarojs/taro'
+import Index from './pages/index'
+
+import './app.scss'
+
+class App extends Component {
+  config: Config = {
+    pages: [
+      'pages/index/index'
+    ],
+    window: {
+      backgroundTextStyle: 'light',
+      navigationBarBackgroundColor: '#fff',
+      navigationBarTitleText: 'WeChat',
+      navigationBarTextStyle: 'black'
+    }
+  }
+
+  componentDidMount () {}
+
+  componentDidShow () {}
+
+  componentDidHide () {}
+
+  componentDidCatchError () {}
+
+  // 在 App 类中的 render() 函数没有实际作用
+  // 请勿修改此函数
+  render () {
+    return (
+      <Index />
+    )
+  }
+}
+
+Taro.render(<App />, document.getElementById('app'))
---
+++ src/pages/index/index.tsx
@@ -0,0 +1,9 @@
+import Taro, { View } from '@tarojs/components';
+
+import './index.scss';
+
+const Index = () => {
+  return <View>Hello World</View>;
+};
+
+export default Index;
