679030f0391b60c9dc8a6a0f16dba1b1a16b9de3
Justin Wang <junting.wang@thoughtworks.com>
2020-04-12 14:29:34 +0800

    [Feat]: add search city

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,5 +1,6 @@
 import Taro, { useEffect, useState } from '@tarojs/taro';
-import { Block, View } from '@tarojs/components';
+import { Block, View, Input } from '@tarojs/components';
+import { BaseEventOrig } from '@tarojs/components/types/common';
 
 import { Header, Footer, TempCard } from '@src/components';
 import { OPEN_WEATHER_API_KEY } from '@src/configs';
@@ -9,16 +10,41 @@ import './index.scss';
 
 const Index = () => {
   const [tempUnit, setTempUnit] = useState<TempUnitType>(TempUnitType.C);
-  const [cityWeatherList, setCityWeatherList] = useState<{ city: string; temp: string }[]>([]);
+  const [searchCity, setSearchCity] = useState<string>('');
+  const [cityWeatherList, setCityWeatherList] = useState<{ id: number; name: string; temp: number }[]>([]);
 
   useEffect(() => {
+    getCurrentWeather('Shanghai');
+  }, []);
+
+  const getCurrentWeather = (city: string) =>
     Taro.request({
-      url: `https://api.openweathermap.org/data/2.5/weather?q=Shanghai&appid=${OPEN_WEATHER_API_KEY}&units=metric`,
+      url: `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${OPEN_WEATHER_API_KEY}&units=metric`,
       method: 'GET',
     }).then((res) => {
-      setCityWeatherList([...cityWeatherList, { city: 'Shanghai', temp: res.data.main.temp }]);
+      const {
+        id,
+        name,
+        main: { temp },
+      } = res.data;
+
+      setCityWeatherList([...cityWeatherList, { id, name, temp }]);
     });
-  }, []);
+
+  const onCityInput = (e: BaseEventOrig<any>) => {
+    setSearchCity(e.detail.value);
+  };
+
+  const onAddWeather = () => {
+    if (!searchCity) {
+      Taro.showToast({ title: 'Please input city', icon: 'none' });
+      return;
+    }
+
+    getCurrentWeather(searchCity).then(() => {
+      setSearchCity('');
+    });
+  };
 
   const onTempUnitChange = (v: TempUnitType) => {
     setTempUnit(v);
@@ -28,14 +54,12 @@ const Index = () => {
     <Block>
       <Header title='Weather Forecast' />
       {cityWeatherList.map((weather) => (
-        <TempCard
-          key={weather.city}
-          location={weather.city}
-          temperature={Math.round(Number(weather.temp))}
-          tempUnit={tempUnit}
-        />
+        <TempCard key={weather.id} location={weather.name} temperature={Math.round(weather.temp)} tempUnit={tempUnit} />
       ))}
-      <View className='add-btn'>Add</View>
+      <View className='add-area-wrap'>
+        <Input placeholder='Search City' value={searchCity} onInput={onCityInput}></Input>
+        <View className='add-btn' onClick={onAddWeather}>Add</View>
+      </View>
       <Footer tempUnit={tempUnit} onTempUnitChange={onTempUnitChange} />
     </Block>
   );
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,9 @@
 import { TempUnitType } from './../types/index';
 
 export const convertTemp = (temp: number, target = TempUnitType.C) => {
+  if (typeof temp === 'string') {
+    temp = Number(temp);
+  }
+
   return target === TempUnitType.F ? Math.round(temp * 1.8 + 32) : Math.round(temp);
 };
